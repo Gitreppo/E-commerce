57789edd6b39bc4b8ec5bdcebae60d2e1720baf9
Gitreppo <125879585+Gitreppo@users.noreply.github.com>
2023-05-04 18:43:58 +0530

    Update App.js

--- Recharts/src/App.js
+++ Recharts/src/App.js
@@ -4,7 +4,6 @@ import Login from './Components/Login';
 import SignUp from './Components/Signup';
 import Main from './Dasboard/Main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Dasboard/Home';
 import Add from './Dasboard/Add';
 import Getall from './Dasboard/Getall';
 
@@ -15,7 +14,6 @@ function App() {
         <Routes>
         <Route exact path='/' element={<SignUp/>} />
         <Route path='/Login' element={<Login/>} />
-        <Route path='/Home' element={<Home />} />
         <Route path='/Add' element={<Add/>} />
         <Route path='/Getall' element={<Getall/>} />
         <Route path='/Main' element={<Main/>} />
